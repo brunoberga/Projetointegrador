10abdc7dc6cddeef7b72be2edb674cbed9a9f0cf
bruno.bpereira2 <bruno.bpereira2@CAS510499W0734.senacsp.edu.br>
2016-05-19 20:08:58 -0300

    Adicionando classes.

---
+++ src/projetointegrador/Vagas.java
@@ -0,0 +1,12 @@
+package projetointegrador;
+
+public class Vagas {
+    
+    public String descricao;
+    //public String empresa; //variável já declarada em DadosEmpresa
+    public Empresa empresa;
+    public String local;
+    public boolean disponivel;
+    public String valor;
+    
+}
